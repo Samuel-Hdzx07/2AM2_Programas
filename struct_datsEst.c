dcf7fdb2289b3a2747972b3620a3daf888c0e45b
Samy <shernandezb2201@alumno.ipn.mx>
2026-02-20 08:07:03 -0600

    Clase 20/02/26

--- struct_datsEst.c
+++ struct_datsEst.c
@@ -1,18 +1,78 @@
-#include <stdio.h>
+    #include <stdio.h>
+    #include <math.h>
 
-struct datosEst{
-    float arreglo[10];
-    int *ptr;
-    int datosIng;
-    float prom;
-    float desvstd;
-};
+    struct datosEst{
+        float arreglo[20];
+        float *ptr;
+        int numDatos;
+        float prom;
+        float desvstd;
+    };
 
-int main(){
-    struct datosEst est;
-    est.ptr = est.arreglo; // Apuntar al inicio del arreglo
+    int meterDatos(struct datosEst *ptrS){
 
-    return 0;
-}
+        printf("Ingrese el numero de datos: ");
+        scanf("%d", &ptrS->numDatos);
 
-//hacer funcion de datos estadisticos con apuntadores, que me calcule las calificaciones del smestre , meter los datos en el arreglo y en datos ing meter las materias , guardar promedio y desviacion estandar]
+        for(ptrS->ptr = ptrS->arreglo; ptrS->ptr < &ptrS->arreglo[ptrS->numDatos]; ptrS->ptr = ptrS->ptr + 1){
+            printf("Ingrese el dato: ");
+            scanf("%f", ptrS->ptr);
+        }
+
+        return 0;
+    }
+
+    int calculosEstadisticos(struct datosEst *ptrS){
+        float sum = 0.0;
+        for(ptrS->ptr= ptrS->arreglo; ptrS->ptr < &ptrS->arreglo[ptrS->numDatos]; ptrS->ptr = ptrS->ptr + 1)
+            sum +=  *ptrS->ptr;
+
+        ptrS->prom = sum / (ptrS->numDatos);
+
+        float sumDesv = 0.0;
+        for(ptrS->ptr= ptrS->arreglo; ptrS->ptr < &ptrS->arreglo[ptrS->numDatos]; ptrS->ptr = ptrS->ptr + 1)
+            sum = sum +  pow((*ptrS->ptr - ptrS->prom), 2);
+
+            sum = sum / (ptrS->numDatos);
+            ptrS->desvstd = pow(sum, 0.5);
+        return 0;
+
+    }
+
+    int menu(){
+        int op;
+        printf("1. Ingresar datos\n");
+        printf("2. Calcular promedio y desviacion estandar\n");
+        printf("3. Salir\n");
+        printf("Ingrese su opcion: ");
+        scanf("%d", &op);
+        return op;
+    }
+
+
+    int main(){
+        struct datosEst datest, *miptrS;
+        miptrS = &datest;
+
+        for(;;){
+            switch(menu()){
+                case 1:
+                    meterDatos(miptrS);
+                    break;
+                case 2:
+                    calculosEstadisticos(miptrS);
+                    printf("Promedio: %.2f\n", miptrS->prom);
+                    printf("Desviacion estandar: %.2f\n", miptrS->desvstd);
+                    break;
+                case 3:
+                    printf("Saliendo del programa...\n");
+                    return 0;
+                default:
+                    printf("Opcion no valida. Intente de nuevo.\n");
+            }
+        }
+
+        return 0;
+    }
+
+    //hacer funcion de datos estadisticos con apuntadores, que me calcule las calificaciones del smestre , meter los datos en el arreglo y en datos ing meter las materias , guardar promedio y desviacion estandar]
